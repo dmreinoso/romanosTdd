9f8e88940164c34de2075cbc52b7c429fc702898
Diego <Diego@DIEGO-DELL>
2015-05-07 14:50:41 -0500

    Commit inicial

---
+++ roman.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of roman
+ *
+ * @author Diego
+ */
+class roman {
+
+    public function convertDigit($arabicNumberToConvert, $one, $five, $ten) {
+        $romanNumber = "";
+        if ($arabicNumberToConvert > 0 && $arabicNumberToConvert <= 3) {
+            for (; $arabicNumberToConvert > 0; $arabicNumberToConvert--) {
+                $romanNumber .= $one;
+            }
+        } elseif ($arabicNumberToConvert == 4) {
+            $romanNumber = "{$one}{$five}";
+        } elseif ($arabicNumberToConvert >= 5 && $arabicNumberToConvert < 9) {
+            $romanNumber = $five;
+            for (; $arabicNumberToConvert > 5; $arabicNumberToConvert--) {
+                $romanNumber .= $one;
+            }
+        } elseif ($arabicNumberToConvert == 9) {
+            $romanNumber = "{$one}{$ten}";
+        }
+        return $romanNumber;
+    }
+
+    public function convertToRoman($arabicNumberToConvert) {
+        $romanNumber = "";
+        $thousands = (int) ($arabicNumberToConvert / 1000);
+        $hundreds = (int) ($arabicNumberToConvert / 100);
+        $tens = (int) ($arabicNumberToConvert / 10) % 10;
+        $romanNumber .= $this->convertDigit($thousands, "M", "", "");
+        $romanNumber .= $this->convertDigit($hundreds, "C", "D", "M");
+        $romanNumber .= $this->convertDigit($tens, "X", "L", "C");
+        $romanNumber .= $this->convertDigit($arabicNumberToConvert % 10, "I", "V", "X");
+        return $romanNumber;
+    }
+
+}
